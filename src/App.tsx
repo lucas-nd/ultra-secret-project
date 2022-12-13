9fd7962d1a4d5ffe5f5c50eeb00d0e0385c216aa
Lucas do Nascimento Diniz <lucas.diniz@nave.com>
2022-12-12 22:48:09 -0300

    first configs

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-1/3">
+        <div className="chat chat-start">
+        <div className="chat-header">
+          any
+        </div>
+          <div className="chat-bubble">Ultra Secret Project will start.</div>
+        </div>
+        <div className="chat chat-end">
+        <div className="chat-header">
+          Lucas
+        </div>
+          <div className="chat-bubble">Finally. We can more!</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
