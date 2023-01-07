bd84c4296d9396f2d070932d1bc66c69c9c1fcfd
Lucas do Nascimento Diniz <lucas.diniz@nave.com>
2023-01-07 20:11:57 -0300

    feat: add react-router-dom lib

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,18 @@
-function App() {
+function Home() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-1/3">
         <div className="chat chat-start">
-        <div className="chat-header">
-          any
-        </div>
+          <div className="chat-header">any</div>
           <div className="chat-bubble">Ultra Secret Project will start.</div>
         </div>
         <div className="chat chat-end">
-        <div className="chat-header">
-          Lucas
-        </div>
+          <div className="chat-header">Lucas</div>
           <div className="chat-bubble">Finally. We can more!</div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default Home;
