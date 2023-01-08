6c5537ba26d54abf5eb8d03e330c9f2e9d4d4981
Lucas do Nascimento Diniz <lucas.diniz@nave.com>
2023-01-08 12:39:14 -0300

    feat(Auth): create login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -16,9 +16,138 @@ const Login = () => {
   return (
     <>
       <div className="w-screen h-screen flex justify-center items-center">
-        <button className="btn btn-primary" onClick={handleLogin}>
-          Login
-        </button>
+        <div className="flex flex-col w-1/4">
+          <h1 className="text-center text-white font-bold mb-4">
+            Ultra Secret Project
+          </h1>
+          <input
+            type="text"
+            placeholder="email"
+            className="input input-bordered input-success w-full mb-4"
+          />
+          <input
+            type="text"
+            placeholder="senha"
+            className="input input-bordered input-success w-full mb-4"
+          />
+          <a className="link no-underline">
+            <h4 className="text-end text-white font-semibold mb-4 hover:text-accent">
+              Esqueceu a senha?
+            </h4>
+          </a>
+          <button className="btn btn-accent" onClick={handleLogin}>
+            Login
+          </button>
+          <div className="divider">ou continue com</div>
+          <div className="flex justify-around mb-4">
+            <button className="btn btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                className="h-6 w-6"
+                viewBox="0 0 48 48"
+              >
+                <defs>
+                  <path
+                    id="a"
+                    d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
+                  />
+                </defs>
+                <clipPath id="b">
+                  <use xlinkHref="#a" overflow="visible" />
+                </clipPath>
+                <path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
+                <path
+                  clip-path="url(#b)"
+                  fill="#EA4335"
+                  d="M0 11l17 13 7-6.1L48 14V0H0z"
+                />
+                <path
+                  clip-path="url(#b)"
+                  fill="#34A853"
+                  d="M0 37l30-23 7.9 1L48 0v48H0z"
+                />
+                <path
+                  clip-path="url(#b)"
+                  fill="#4285F4"
+                  d="M48 48L17 24l-4-3 35-10z"
+                />
+              </svg>
+            </button>
+            <button className="btn btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                className="h-6 w-6"
+                viewBox="0 0 48 48"
+              >
+                <defs>
+                  <path
+                    id="a"
+                    d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
+                  />
+                </defs>
+                <clipPath id="b">
+                  <use xlinkHref="#a" overflow="visible" />
+                </clipPath>
+                <path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
+                <path
+                  clip-path="url(#b)"
+                  fill="#EA4335"
+                  d="M0 11l17 13 7-6.1L48 14V0H0z"
+                />
+                <path
+                  clip-path="url(#b)"
+                  fill="#34A853"
+                  d="M0 37l30-23 7.9 1L48 0v48H0z"
+                />
+                <path
+                  clip-path="url(#b)"
+                  fill="#4285F4"
+                  d="M48 48L17 24l-4-3 35-10z"
+                />
+              </svg>
+            </button>
+            <button className="btn btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                className="h-6 w-6"
+                viewBox="0 0 48 48"
+              >
+                <defs>
+                  <path
+                    id="a"
+                    d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
+                  />
+                </defs>
+                <clipPath id="b">
+                  <use xlinkHref="#a" overflow="visible" />
+                </clipPath>
+                <path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
+                <path
+                  clip-path="url(#b)"
+                  fill="#EA4335"
+                  d="M0 11l17 13 7-6.1L48 14V0H0z"
+                />
+                <path
+                  clip-path="url(#b)"
+                  fill="#34A853"
+                  d="M0 37l30-23 7.9 1L48 0v48H0z"
+                />
+                <path
+                  clip-path="url(#b)"
+                  fill="#4285F4"
+                  d="M48 48L17 24l-4-3 35-10z"
+                />
+              </svg>
+            </button>
+          </div>
+          <h4 className="text-center text-white font-medium">
+            Não possui conta?{" "}
+            <a className="link no-underline hover:text-accent">Clique aqui</a>
+          </h4>
+        </div>
       </div>
     </>
   );
